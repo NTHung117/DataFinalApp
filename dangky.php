a25c279decc9031af7a714eddc3d5b37a6425adf
NTHung117 <nguyentienhung117@gmail.com>
2023-04-02 01:41:16 +0700

    dangky.php

--- dangky.php
+++ dangky.php
@@ -5,22 +5,34 @@ $pass = $_POST['pass'];
 $username = $_POST['username'];
 $numberPhone = $_POST['numberPhone'];
 
-$query = 'INSERT INTO `user`(`email`, `pass`, `username`, `numberPhone`) VALUES ("'.$email.'","'.$pass.'","'.$username.'","'.$numberPhone.'") ';
+//Kiểm tra email có bị trùng trên data
+$query = 'SELECT * FROM `user` WHERE `email` = "'.$email.'" ';
 $data = mysqli_query($conn, $query);
-
-
-if ($data == true)  {
-
-    $arr = [
-        'success' => true,
-        'message' => "Thanh cong"
-    ];
-}else {
+$numrow = mysqli_num_rows($data);//Hàm này kiểm tra xem có giữ liệu trả về không
+if ($numrow > 0) {//Nếu numrow > 0 là đã có dữ liệu trả về
     $arr = [
         'success' => false,
-        'message' => "Khong thanh cong"
+        'message' => "Email da ton tai"
     ];
+}else{
+        $query = 'INSERT INTO `user`(`email`, `pass`, `username`, `numberPhone`) VALUES ("'.$email.'","'.$pass.'","'.$username.'","'.$numberPhone.'") ';
+        $data = mysqli_query($conn, $query);
+        if ($data == true)  {
+
+            $arr = [
+                'success' => true,
+                'message' => "Thanh cong"
+            ];
+        }else {
+            $arr = [
+                'success' => false,
+                'message' => "Khong thanh cong"
+            ];
+        }
 }
+
+
+
 
 print_r(json_encode($arr));
 
